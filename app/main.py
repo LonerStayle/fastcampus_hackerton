a4c6e0c9f2a146cf1c13d66ef2ef80acb354b3ce
seobi <dlwlstjq410@gmail.com>
2026-02-01 03:44:58 +0900

    add

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routers import ai_router, test_router
+from fastapi.middleware.cors import CORSMiddleware
 # from app.core.database import init_vector_extension
 
 
@@ -13,7 +14,13 @@ from app.routers import ai_router, test_router
 app = FastAPI()
 app.include_router(test_router.router)
 app.include_router(ai_router.router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # 이 부분이 5173 포트 허용
+    allow_credentials=True,  # 쿠키/인증 헤더 허용 (필요 시)
+    allow_methods=["*"],     # 모든 HTTP 메서드 (GET, POST 등) 허용
+    allow_headers=["*"],     # 모든 헤더 허용
+)
 
 @app.get("/")
 async def read_root():
